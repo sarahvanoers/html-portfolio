1ea747147b8c7b7c7af30b390e2fcbcd64ad14fd
Sarah Van Oers <sarahvanoers@icloud.com>
2025-09-13 10:42:49 +0200

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -4,209 +4,209 @@ const paintings = {
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G1_A_1600.jpg"
+    image: "assets/G1_detail.jpg"
   },
   "G2": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G2_A_1600.jpg"
+    image: "assets/G2_detail.jpg"
   },
   "G3": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G3_A_1600.jpg"
+    image: "assets/G3_detail.jpg"
   },
   "G4": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G4_A_1600.jpg"
+    image: "assets/G4_detail.jpg"
   },
   "G5": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G5_A_1600.jpg"
+    image: "assets/G5_detail.jpg"
   },
   "G6": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G6_A_1600.jpg"
+    image: "assets/G6_detail.jpg"
   },
   "G7": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G7_A_1600.jpg"
+    image: "assets/G7_detail.jpg"
   },
   "G8": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G8_A_1600.jpg"
+    image: "assets/G8_detail.jpg"
   },
   "G9": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G9_A_1600.jpg"
+    image: "assets/G9_detail.jpg"
   },
   "G10": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G10_A_1600.jpg"
+    image: "assets/G10_detail.jpg"
   },
   "G11": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G11_A_1600.jpg"
+    image: "assets/G11_detail.jpg"
   },
   "G12": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G12_A_1600.jpg"
+    image: "assets/G12_detail.jpg"
   },
   "G13": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G13_A_1600.jpg"
+    image: "assets/G13_detail.jpg"
   },
   "G14": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G14_A_1600.jpg"
+    image: "assets/G14_detail.jpg"
   },
   "G15": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G15_A_1600.jpg"
+    image: "assets/G15_detail.jpg"
   },
   "G16": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G16_A_1600.jpg"
+    image: "assets/G16_detail.jpg"
   },
   "G17": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G17_A_1600.jpg"
+    image: "assets/G17_detail.jpg"
   },
   "G18": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G18_A_1600.jpg"
+    image: "assets/G18_detail.jpg"
   },
   "G19": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G19_A_1600.jpg"
+    image: "assets/G19_detail.jpg"
   },
   "G20": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G20_A_1600.jpg"
+    image: "assets/G20_detail.jpg"
   },
   "G21": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G21_A_1600.jpg"
+    image: "assets/G21_detail.jpg"
   },
   "G22": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G22_A_1600.jpg"
+    image: "assets/G22_detail.jpg"
   },
   "G23": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G23_A_1600.jpg"
+    image: "assets/G23_detail.jpg"
   },
   "G24": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G24_A_1600.jpg"
+    image: "assets/G24_detail.jpg"
   },
   "G25": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G25_A_1600.jpg"
+    image: "assets/G25_detail.jpg"
   },
   "G26": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G26_A_1600.jpg"
+    image: "assets/G26_detail.jpg"
   },
   "G27": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G27_A_1600.jpg"
+    image: "assets/G27_detail.jpg"
   },
   "G28": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G28_A_1600.jpg"
+    image: "assets/G28_detail.jpg"
   },
   "G29": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G29_A_1600.jpg"
+    image: "assets/G29_detail.jpg"
   },
   "G30": {
     title: "Untitled",
     year: "2023",
     medium: "Acrylic on canvas",
     size: "00 x 00 cm",
-    image: "assets/G30_A_1600.jpg"
+    image: "assets/G30_detail.jpg"
   }
 };
