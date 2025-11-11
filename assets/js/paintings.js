5e62c49e878cc382a9dce9de2d684cee334210e6
Sarah Van Oers <sarahvanoers@icloud.com>
2025-11-11 09:50:24 +0100

    Delete assets/js directory

--- assets/js/paintings.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("[data-painting-id]").forEach(el => {
-  const id = el.getAttribute("data-painting-id");
-  const painting = paintings[id];
-  if (painting) {
-    el.querySelector(".painting-title").textContent = painting.title + ", " + painting.year + " →";
-    el.querySelector("img").alt = painting.title + " by Guy Van Oers";
-    el.querySelector("img").src = "assets/" + id + "_detail.jpg";
-  }
-});
